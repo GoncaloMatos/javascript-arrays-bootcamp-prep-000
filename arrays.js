96c673a87fb4169d81a5ac7a79baf027703f1347
Gonçalo Matos <matos98.ff@gmail.com>
2018-09-03 08:08:57 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,4 +2,6 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
   function addElementToBeginningOfArray (array, element) {
   const a = array;
+  a.unshift(element);
+  return a;
   }
