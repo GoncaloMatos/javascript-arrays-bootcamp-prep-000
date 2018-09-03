e0fd166af3fe4c04134771f46baca8e61f5e5e05
Gonçalo Matos <matos98.ff@gmail.com>
2018-09-03 08:26:14 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,22 +1,27 @@
-var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+  var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
   function addElementToBeginningOfArray (array, element) {
     const a = array;
     const b = [element, ...array];
     return b;
   }
+  
   function destructivelyAddElementToBeginningOfArray (array, element) {
     var c = array;
     c.unshift(element);
     return c;
   }
+  
   function addElementToEndOfArray (array, element) {
     const d = array;
     const e = [...array, element];
     return e;
   }
+  
   function destructivelyAddElementToEndOfArray (array, element) {
     var f = array;
     f.push(element);
     return f
   }
+  
+  
